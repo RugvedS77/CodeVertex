17635fe120b9ba62133eba8ffa24875b57d51aef
RugvedS77 <rugvedsawant2510@gmail.com>
2023-12-30 14:17:17 +0530

    modified

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,3 @@
-import string
-
 def Sumation(a=0,b=0):
     print("Addition of",a,"and",b,"is: ",a+b) 
 def Subtract(a=0,b=0):
@@ -8,11 +6,13 @@ def Multiply(a=0,b=0):
     print("Multiplication of",a,"and",b,"is: ",a*b)
 def Divide(a=0,b=0):
     print("Division of",a,"and",b,"is: ",round(a/b,3))
-
-def ValidateNum(a):
-    while(a.isnumeric()):
+    
+def ValidateNum(num):
+    try:
+        float(num)
         return True
-    else:
+    
+    except ValueError:
         return False
 
 print("Welcome...")
@@ -21,18 +21,16 @@ print("2.Subtraction")
 print("3.Mutiplication")
 print("4.Division")
 
-# choice=int(input("please enter the opertion you want: "))
-
 while True :
-    choice=input("please enter the opertion you want: ")
-    if ValidateNum(choice) :
-        choice=int(choice)
-        num1=input("Enter the first number: ")
-        if ValidateNum(num1):
-            num1=float(num1)
-            num2=input("Enter the second number: ")
-            if ValidateNum(num2):
-                num2=float(num2)
+    num1=input("Enter the first number: ")
+    if ValidateNum(num1):
+        num2=input("Enter the second number: ")
+        if ValidateNum(num2):
+            choice=input("please enter the opertion you want: ")
+            if choice.isdigit() and 1 <= int(choice) <= 4:
+                num1 = float(num1)
+                num2 = float(num2)
+                choice = int(choice)
                 print("Calcutating...")
                 break            
             else:
--- Password.py
+++ Password.py
@@ -14,18 +14,20 @@ def Complexity(str):
         characters=string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation
     return characters
 
-print("WELCOME...to Password Generetor...")
+print("WELCOME...to Password Generator...")
 while True:
     passLength=input("Enter length for password: ")
     if passLength.isnumeric():
        passLength=int(passLength)
        if passLength>0 and passLength<=40:
-
             level=input("Enter the level of complexity of password:\nlow\nmoderate\nhigh\n: ")
-            characters=Complexity(level)
-            password=CreatePassword(passLength,characters)
-            print("password is: ",password)
-            break
+            if level =="low" or level=='moderate' or level=='high':
+                characters=Complexity(level)
+                password=CreatePassword(passLength,characters)
+                print("password is: ",password)
+                break
+            else:
+                print("Invalid choice..")
        else:
            print("Please enter a natural number in range of (1-40)")
     else:
